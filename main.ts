e5cabbc09f393c5ae0afdef7dcb52136b37b84a7
Frauschinkelmani <121432397+FrauSchinkelmani@users.noreply.github.com>
2023-01-22 14:11:49 +0100

    fixed the scissors icon

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ input.onGesture(Gesture.Shake, function () {
         basic.showLeds(`
             # # . . #
             # # . # .
-            . . . . .
+            . . # . .
             # # . # .
             # # . . #
             `)
