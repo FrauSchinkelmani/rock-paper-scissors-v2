6977412131d20dff7a989c4f8435e00169ef3f49
Frauschinkelmani <121432397+FrauSchinkelmani@users.noreply.github.com>
2023-01-22 14:08:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 523, 1, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 300, 200, 255, 0, 75, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . . . .
+            # # . # .
+            # # . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 1, 255, 0, 10, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
+basic.forever(function () {
+	
+})
